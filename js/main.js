2cf9747f20d610037a6e7da322deeb847f5323f6
yuukikashiwagi <yuuki_kashiwagi@demia.co.jp>
2024-11-07 22:31:08 +0900

    レーン移動

--- js/main.js
+++ js/main.js
@@ -274,14 +274,22 @@ document.addEventListener("DOMContentLoaded", function () {
 
 // プレイヤーの移動
 function move(){
-    // player.position.z -= 0.2;
-    if (gamma > 20) {
-        index += 1;
-        player.position.x = course[index];
-    } else if (gamma < -20) {
-        index -= 1;
-        player.position.x = course[index];
-    }
+    player.position.z -= 0.2;
+    if (gamma > 20 && !isMoving) {
+        if (index == 0 || index == 1) {
+          isMoving = true;
+          index += 1;
+          player.position.x = course[index];
+        }
+      } else if (gamma < -20 && !isMoving) {
+        if (index == 1 || index == 2) {
+          isMoving = true;
+          index -= 1;
+          player.position.x = course[index];
+        }
+      } else if (gamma < 1.5 && gamma > -1.5) {
+        isMoving = false;
+      }
 }
 
 // プレイヤーのジャンプ
