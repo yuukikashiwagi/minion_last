54af6898169b17b69794625749fa71dbc3ba406c
yuukikashiwagi <yuuki_kashiwagi@demia.co.jp>
2024-11-07 22:35:27 +0900

    ジャンプ実装

--- js/main.js
+++ js/main.js
@@ -293,9 +293,19 @@ function move(){
 }
 
 // プレイヤーのジャンプ
-function jump(){
-    // ここに追加
-}
+function jump() {
+    if (!isJumping && aZ > 0) {
+      player_v_y = initial_velocity;
+      isJumping = true;
+    } else if (isJumping) {
+      player_v_y -= gravity;
+      player.position.y += player_v_y;
+      if (player.position.y <= 0) {
+        isJumping = false;
+        player.position.y = 0;
+      }
+    }
+  }
 
 // 衝突判定
 function collision(){
@@ -340,7 +350,7 @@ function animate(){
     move();
 
     // ジャンプ関数の実行
-    // ここに追加
+    jump();
 
     // 衝突判定関数の実行
     // ここに追加
